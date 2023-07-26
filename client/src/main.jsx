2ae24dcfb252689fe540e8ebafb6d4be3f44ba42
CamCufre <cufrecamila1@gmail.com>
2023-07-26 19:10:07 -0300

    a

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 import axios from 'axios';
 
 // axios.defaults.baseURL = `http://localhost:3001`;
-axios.defaults.baseURL = `https://aprende-conmigo-production.up.railway.app:3001`;
+axios.defaults.baseURL = `aprender-conmig-server-production.up.railway.app:3001`;
 
 ReactDOM.createRoot(
   document.getElementById('root')

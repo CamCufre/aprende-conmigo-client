dcb90fb1826f840305cf5614f0f0e7615378a226
CamCufre <cufrecamila1@gmail.com>
2023-07-25 22:02:39 -0300

    client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import store from './Redux/store';
 import App from './App';
 import './index.css';
+import axios from 'axios';
+
+axios.defaults.baseURL = `https://aprender-conmig-server-production.up.railway.app/`
 
 ReactDOM.createRoot(
   document.getElementById('root')
